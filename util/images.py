02764502b4eb32c9d97e70c654d364d88e26f255
Alexander Clarke <alex-p-clarke@hotmail.com>
2022-11-12 15:52:22 -0500

    Implemented the display, and reading/writing images from mongo

--- util/images.py
+++ util/images.py
@@ -1,4 +1,6 @@
-import pymongo
+import re           # For string parsing.
+import datetime
+
 '''
     Version: 1.0
     Author: Utility Team
@@ -27,3 +29,38 @@ def image(db, scheduled, start, end, priority, playlist, duration, uuid):
     posts = db.posts
     post_id = posts.insert_one(post).inserted_id
     return post_id
+
+
+
+
+
+# --- SETTINGS --- #
+
+default_duration = 30
+
+# --- IMAGE INSERT --- #
+
+def insert_image(db, fs, path, schedule, duration=default_duration):
+  # Get the correct collection.
+  col = db["images"]
+
+  # Parse the file name.
+  filename = re.search("[^/]*$", path)[0]
+
+  # Insert the image file into GridFS.
+  with open(path, 'rb') as f:
+    contents = f.read()
+  img_fsid = fs.put(contents, filename=filename)
+
+  # Define what the image document will look like.
+  img = {
+    "filename" : filename,
+    "file_type" : re.search("[^\.]*$", filename)[0],
+    "duration" : duration,
+    "file_id" : img_fsid,
+    "date_added" : datetime.datetime.utcnow()
+  }
+
+  # Push the image document to the images collection.
+  post_id = col.insert_one(img)
+  return post_id
