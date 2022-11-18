4f366412b98c07eb0df7036f2867448dcd110982
Derek Wilson <dcwilson0901@gmail.com>
2022-11-18 10:33:45 -0500

    Created pages to start work on

--- client/src/components/playlistList/tempPlaylists.js
+++ client/src/components/playlistList/tempPlaylists.js
@@ -1,7 +1,7 @@
-const media = [
+const playlists = [
   {
     id: 1,
-    name: 'Day Change Sign',
+    name: 'Daylight Savings Time',
     description: 'For when daylight savings hits',
     start_date: '2022-11-14',
     end_date: '2022-11-20',
@@ -10,22 +10,22 @@ const media = [
   },
   {
     id: 2,
-    name: "Fenwick's Best Sign",
-    description: "Everybody's gotta know!",
-    start_date: '2022-11-18',
-    end_date: '2022-11-22',
+    name: 'App Basketball',
+    description: 'For when daylight savings hits',
+    start_date: '2022-11-14',
+    end_date: '2022-11-20',
     image:
       'https://www.8newsnow.com/wp-content/uploads/sites/59/2021/11/daylight_saving_time_fall_back_graphic.jpg?w=1752&h=986&crop=1'
   },
   {
     id: 3,
-    name: 'Yippeeeeee',
-    description: 'Wahoooooooo',
-    start_date: '2022-11-12',
-    end_date: '2022-11-14',
+    name: 'Very Cool Playlist',
+    description: 'For when daylight savings hits',
+    start_date: '2022-11-14',
+    end_date: '2022-11-20',
     image:
       'https://www.8newsnow.com/wp-content/uploads/sites/59/2021/11/daylight_saving_time_fall_back_graphic.jpg?w=1752&h=986&crop=1'
   }
 ];
 
-export default media;
+export default playlists;
---
+++ client/src/pages/public/playlists/Playlists.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PlaylistList from '../../../components/playlistList/PlaylistList';
+
+class Playlists extends Component {
+  render() {
+    return (
+      <div>
+        <PlaylistList />
+      </div>
+    );
+  }
+}
+
+export default Playlists;
