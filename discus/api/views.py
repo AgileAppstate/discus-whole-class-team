0782aea7747e6b2be384a5cebd3f358467af323f
t9605tripp <55712425+t9605tripp@users.noreply.github.com>
2022-11-21 20:52:01 -0500

    test api call in ping

--- discus/api/views.py
+++ discus/api/views.py
@@ -1,10 +1,22 @@
-from flask import Flask
-from discus.util import playlists
+from flask import Flask, jsonify, request
+from discus.util import *
+#from discus.util import images
 from discus.api import app
-
-# db.setup() will have to be run before any actions with the database
+from bson.json_util import dumps
+#from discus.util import db
 
 @app.route("/ping", methods=["GET"])
 def status():
     playlists.playlist_insert("helloworld")
-    return {"msg": "pong"}
+    live_images = []
+    cursor = db.images.find()
+    list_cursor = list(cursor)
+    json_data = dumps(list_cursor, indent=4)
+    return json_data
+
+@app.route("/get_image_named<string:image_name>", methods=["GET"])
+def get_image_named(name):
+    return images.image_get_by_name(name)
+
+
+def collection_tojson()
