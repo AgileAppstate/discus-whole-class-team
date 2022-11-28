fb1704e47371ce758ea40ca7e83065637743943d
Alexander Clarke <alex-p-clarke@hotmail.com>
2022-11-28 12:18:02 -0500

    small fixes

--- util/channels.py
+++ util/channels.py
@@ -1,6 +1,6 @@
 # --- IMPORTS --- #
 
-import datetime
+from datetime import datetime, timedelta
 import db
 
 # --- FUNCTIONS --- #
@@ -12,7 +12,7 @@ def channel_insert(chanName, playlistID=None):
         "name" : chanName,
         "playlist" : playlistID,
         "mode" : None,
-        "date_created" : datetime.datetime.utcnow()
+        "date_created" : datetime.utcnow()
     }
     
     # Push the channel document to the channels collection.
@@ -26,3 +26,7 @@ def channel_get_by_name(chanName):
 
 def channel_set_playlist(chanID, playlistID):
     db.channels.update_one({ "_id": chanID }, { "$set": { "playlist": playlistID } }) # set playlist for channel
+
+def channel_next_swap():
+    # return datetime.utcnow() + timedelta(minutes=30)
+    return db.channels.find_one({"start_date" : {'$gte' : datetime.utcnow()}})["start_date"]
