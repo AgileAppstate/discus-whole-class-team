6d390a09212ac81e4e147fd76ab8bedf2661330f
KittenMoon <47439334+KittenMoon@users.noreply.github.com>
2022-11-28 10:46:29 -0500

    Added setters for most fields in playlist, img, and chan

--- util/channels.py
+++ util/channels.py
@@ -6,12 +6,15 @@ import db
 # --- FUNCTIONS --- #
 
 # creates a new channel
-def channel_insert(chanName, playlistID=None):
+def channel_insert(chanName, playlistID=None, mode="Single", recurringInfo=None, startTime=None, endTime=None):
     # Define what the channel document will look like.
     chan = {
         "name" : chanName,
         "playlist" : playlistID,
-        "mode" : None,
+        "mode" : mode,
+        "recurring_info" : recurringInfo,
+        "start_time" : startTime,
+        "end_time" : endTime,
         "date_created" : datetime.datetime.utcnow()
     }
     
@@ -19,10 +22,27 @@ def channel_insert(chanName, playlistID=None):
     post_id = db.channels.insert_one(chan)
     return post_id.inserted_id
 
-
+# get channel by name
 def channel_get_by_name(chanName):
     return db.channels.find_one({"name" : chanName})["_id"]
 
+# deletes the channel
+def channel_delete(id):
+    # Delete the channel document.
+    db.channels.delete(id)
 
+# sets the playlist for the channel
 def channel_set_playlist(chanID, playlistID):
     db.channels.update_one({ "_id": chanID }, { "$set": { "playlist": playlistID } }) # set playlist for channel
+
+# sets the mode for the channel
+def channel_set_mode(chanID, mode, recurringInfo=None):
+    db.channels.update_one({ "_id": chanID }, { "$set": { "mode": mode, "recurring_info": recurringInfo } }) # set mode for channel
+
+# sets the start time for the channel
+def channel_set_start_time(chanID, startTime):
+    db.channels.update_one({ "_id": chanID }, { "$set": { "start_time": startTime } }) # set start time for channel
+
+# sets the end time for the channel
+def channel_set_end_time(chanID, endTime):
+    db.channels.update_one({ "_id": chanID }, { "$set": { "end_time": endTime } }) # set end time for channel
--- util/playlists.py
+++ util/playlists.py
@@ -6,12 +6,12 @@ import db
 # --- FUNCTIONS --- #
 
 # creates a new playlist
-def playlist_insert(playlistname, shuffle=False):
+def playlist_insert(playlistname, shuffle=False, itemList=[]):
 
     # Define what the playlist document will look like.
     plst = {
         "name" : playlistname,
-        "items" : [],
+        "items" : itemList,
         "shuffle" : shuffle,
         "date_created" : datetime.datetime.utcnow()
     }
@@ -25,8 +25,28 @@ def playlist_get_by_name(playlistname):
 
 # adds image or playlist to playlist
 def playlist_insert_item(playlistID, itemID, itemType):
+    if itemID == playlistID:
+        return
+    # TODO: check if itemID is already in playlist
     db.playlistts.update_one({ "_id": playlistID }, { "$push": { "items": {"type" : itemType, "objectID" : itemID } } }) # add item to playlist
 
 # removes image or playlist from playlist
 def playlist_remove_item(playlistID, itemID):
     db.playlists.update_one({ "_id": playlistID }, { "$pull": { "items": {"objectID" : itemID } } }) # remove item from playlist
+
+# deletes the playlist
+def playlist_delete(id):
+    # Delete the references to this playlist in any playlists.
+    for plst in db.playlists.find({"items": id}):
+        playlist_remove_item(plst["_id"], id)
+
+    # Delete the playlist document.
+    db.playlists.delete(id)
+
+# sets the shuffle mode for the playlist
+def playlist_set_shuffle(id, shuffle):
+    db.playlists.update_one({ "_id": id }, { "$set": { "shuffle": shuffle } }) # set shuffle for playlist
+
+# sets the name for the playlist
+def playlist_set_name(id, name):
+    db.playlists.update_one({ "_id": id }, { "$set": { "name": name } }) # set name for playlist
