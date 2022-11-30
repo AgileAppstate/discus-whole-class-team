bb97b4f47615a65026cb89fc48dc6de58df6ac0d
Phillip Davis <phdavis1027@gmail.com>
2022-11-30 12:09:27 -0500

    impl Thom's feedback

--- discus/api/views.py
+++ discus/api/views.py
@@ -1,14 +1,18 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from discus.util import playlists
 from discus.api import app
 
 # db.setup() will have to be run before any actions with the database
 
+@app.route('/ping', methods=['GET']):
+    playlists.playlist_insert('helloworld')
+    return {'msg':'pong'}
+
 @app.route("/playlists", methods=["POST"])
 def add_playlist():
     playlist = request.args.get('playlist')
     playlists.playlist_insert(playlist)
+    return {}
 
 @app.route('/playlists/insert', methods=["POST"])
 def insert_into_playlist():
