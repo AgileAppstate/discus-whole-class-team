b6341c2de0ddd94fcc1af9c86701ce7c8dde290f
Phillip Davis <phdavis1027@gmail.com>
2022-12-07 15:37:30 -0500

    wip: Fix CLI runner, specify subcommand structure
    Currently the issue is that Click-Shell is trying to pass arguments
    to "image," which should just be a container for subcommands.
    I think it will take a hack to make this work, as for some reason
    this is not the expected use.

--- discus/api/views.py
+++ discus/api/views.py
@@ -1,12 +1,48 @@
 from flask import Flask, request
 from discus.util import playlists
+from discus.util import images
+from discus.util import channels
 from discus.api import app
+from bson.json_util import dumps
 
+#dropzone web,
 # db.setup() will have to be run before any actions with the database
 
-@app.route('/ping', methods=['GET']):
-    playlists.playlist_insert('helloworld')
-    return {'msg':'pong'}
+#BASE_64 convert the images
+
+@app.route('/ping', methods=['POST'])
+def status():
+    image_ids = request.args.get('id')
+    return {'msg':image_ids}
+
+#get all records
+@app.route("/get_collection_<string:coll_name>", methods=["GET"])
+def get_collection(coll_name):
+    json_data = {}
+    # We should use a generic syntax like def collection_tojson(collectionName)
+    if (coll_name == 'playlists'):
+        cursor = playlists.playlist_get_all()
+        json_data = cursor_to_json(cursor)
+    elif (coll_name == 'images'):
+        cursor = images.image_get_all()
+        json_data = cursor_to_json(cursor)
+    elif (coll_name == 'channels'):
+        cursor = channels.channel_get_all()
+        json_data = cursor_to_json(cursor)
+    return json_data
+
+#Get a single record or multiple records, by id
+#how to pass multiple records
+#636ff99df79d9f60de9d05a4
+#636ff99cf79d9f60de9d05a1
+'''
+@app.route("/get_row_<images>_<1,2,3,5,8>", methods=)
+def get_rows()
+
+#For image insert, find where to save
+@app.route("")
+
+'''
 
 @app.route("/playlists", methods=["POST"])
 def add_playlist():
@@ -20,3 +56,32 @@ def insert_into_playlist():
     item_id = request.args.get('item_id')
     item_type = request.args.get('item_type')
     playlists.playlist_insert_item(playlist, item_id, item_type)
+
+def cursor_to_json(cursor):
+    list_cursor = list(cursor)
+    json_data = dumps(list_cursor, indent=4)
+    return json_data
+
+#@app.route("/get_collection_<string:coll_name>", methods=["GET"])
+#def get_collection(coll_name):
+#    return collection_tojson(coll_name)
+
+
+
+#TODO
+#all the table
+
+#RETURN N RECORDS (table_name, id_list)
+
+#INSERT (no id from them) single json object or a list of json objects
+
+#UPDATE record, list of records
+
+#DELETE single id or a list of ids and a table name
+
+#def collection_tojson(collectionName):
+#    discus = db.get_db()
+#    collection = discus.get_collection(collectionName)
+#    cursor = collection.find()
+
+
--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 import sys
 
+argv = sys.argv
+if len(argv) < 2: # No argument
+    print("usage: run.py <util | api | cli>")
+    sys.exit(1)
+
 if sys.argv[1] == "util":
     from discus.util import main
 elif sys.argv[1] == "api":
     from discus.api.views import app
     app.run(debug=True, port=8080)
+elif sys.argv[1] == "cli":
+    import discus.cli.main as main
+    main() 
+
