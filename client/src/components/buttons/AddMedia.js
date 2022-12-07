0943fcf277b21404bc0f6447b735952274d8a66c
Derek Wilson <dcwilson0901@gmail.com>
2022-12-06 21:42:57 -0500

    Added view items button, but it doesn't format correctly yet

--- client/src/components/buttons/AddMedia.js
+++ client/src/components/buttons/AddMedia.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Dialog from '../dialogs/AddMediaDialog';
+import PropTypes from 'prop-types';
 import 'reactjs-popup/dist/index.css';
 
 class AddMedia extends React.Component {
+
   handleItemAdd = (event) => {
     this.props.onChange(event);
   }
@@ -11,5 +13,10 @@ class AddMedia extends React.Component {
     return <Dialog onChange={this.handleItemAdd}></Dialog>;
   }
 }
+
+AddMedia.propTypes = {
+  children: PropTypes.any,
+  onChange: PropTypes.func,
+};
 
 export default AddMedia;
--- client/src/components/playlistList/tempPlaylists.js
+++ client/src/components/playlistList/tempPlaylists.js
@@ -2,21 +2,21 @@ const playlists = [
   {
     id: 1,
     name: 'Daylight Savings Time',
-    items: 'For when daylight savings hits',
+    items: [1, 2, 3],
     shuffle: false,
     date_created: '2022-11-14',
   },
   {
     id: 2,
     name: 'Playlist Of Motivational Posters',
-    items: 'For when daylight savings hits',
+    items: 'So motivational!',
     shuffle: true,
     date_created: '2022-10-14',
   },
   {
     id: 3,
     name: 'Very Cool Playlist',
-    items: 'For when daylight savings hits',
+    items: 'So cool...',
     shuffle: true,
     date_created: '2022-10-25',
   }
