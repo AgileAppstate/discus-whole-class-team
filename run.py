9885595645aeecd240ba9caef0e20fc2b492d80c
t9605tripp <55712425+t9605tripp@users.noreply.github.com>
2022-12-07 15:45:56 -0500

    update api port

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ if sys.argv[1] == "util":
     from discus.util import main
 elif sys.argv[1] == "api":
     from discus.api.views import app
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8000)
