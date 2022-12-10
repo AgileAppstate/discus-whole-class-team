699471bd138ee046c02e5da62bd6989a75453314
KittenMoon <47439334+KittenMoon@users.noreply.github.com>
2022-12-09 23:37:24 -0500

    🇮 🇦🇲 🇫🇮🇽🇮🇳🇬 🇹🇭🇮🇸 🇧🇷🇴🇰🇪🇳 🇵🇦🇷🇪🇳🇹🇭🇪🇸🇮🇸 ;)

--- discus/util/db.py
+++ discus/util/db.py
@@ -47,7 +47,7 @@ def setup(attempts=3):
     playlists = client_discus["playlists"]
     channels = client_discus["channels"]
     client_gridfs = client["gridfs"]
-    fs = gridfs.GridFS(client_gridfs
+    fs = gridfs.GridFS(client_gridfs)
 
 # Method to close connection to db
 def close(client):
